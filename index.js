f6f8c3565537326174c5dc11d9a0fbfa697f985b
machao <machao196@126.com>
2022-12-10 18:02:51 +0800

    init

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ window.onload = ()=>{
 }
 
 var loader = {
-    rootPath:'http://127.0.0.1:8004/',
+    rootPath:'https://mcok.github.io/alist-web-media/',
     loadScript(src){
         return new Promise((resolve,reject)=>{
             var script = document.createElement('script');
